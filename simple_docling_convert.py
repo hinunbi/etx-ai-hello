9541c5bef6ad7a4c8c1ca8eb8370fe5e9e834e1f
Surote Wongpaiboon <surote.z9@gmail.com>
2025-05-06 15:04:26 +0800

    fix: output file

--- simple_docling_convert.py
+++ simple_docling_convert.py
@@ -3,6 +3,6 @@ from docling.document_converter import DocumentConverter
 source = "https://access.redhat.com/sites/default/files/attachments/openshift_virtualization_reference_implementation_guide.pdf"  # document per local path or URL
 converter = DocumentConverter()
 result = converter.convert(source)
-with open("output.md", "w") as file:
+with open("knowledge.md", "w") as file:
 	file.write(result.document.export_to_markdown())
 
